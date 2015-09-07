8222d7ec617062d4f78cae15f8016eeb47e1894e
ГККрумов <geobojur@abv.bg>
2015-09-07 13:28:25 +0300

    Модул 02, Видео 03 - Основни оператори за целочислен тип данни

--- Модул02/Модул02/Program.cs
+++ Модул02/Модул02/Program.cs
@@ -11,17 +11,38 @@ namespace Модул02
 			int sum = 0;
 
 			//Събиране на числа
-			a = 55;
-			b = 10;
+			a = 10;
+			b = 5;
 
 			sum = a + b;
 		
 			//Писане в конзола
-			Console.Write ( a );
+			Console.Write (a);
 			Console.Write (" + ");
-			Console.Write ( b );
+			Console.Write (b);
 			Console.Write (" = ");
-			Console.WriteLine ( sum );
+			Console.WriteLine (sum);
+
+			//Допълнителни оператори
+			Console.WriteLine ("\nИзползване на +=");
+			a += b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на -=");
+			a -= b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на *=");
+			a *= b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на ++");
+			a++;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("Стандартно делене" + (16 / 3).ToString ());
+			Console.WriteLine ("Остатък от делене" + (16 % 3).ToString ());
+
 		}
 	}
 }
