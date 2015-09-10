16b903c8f190e56046ce172e3f645391bddc4341
ГККрумов <geobojur@abv.bg>
2015-09-10 11:31:02 +0300

    Модул 02, Видео 08

--- Модул02/TableProject/Program.cs
+++ Модул02/TableProject/Program.cs
@@ -6,20 +6,49 @@ namespace TableProject
 	{
 		public static void Main (string[] args)
 		{
-			string[] row = new string[3];
 
-			row [0] = "Ред 1";
-			row [1] = "Ред 2";
-			row [2] = "Ред 3";
+			//Дефиниране на таблица
+			string[,] table = new string[2, 3];
 
-			Console.WriteLine ("Масив row: " + row [0] + ", " + row [1] + ", " + row [2] + "\n");
+			//Въвеждане на първи ред
+			table [0, 0] = "Георги"; table [0, 1] = "Крумов"; table [0, 2] = "71";
 
-			String[] pars = "1,2,3,4,5,6,7,8,9".Split (',');
-			Console.WriteLine ("Числата на масива:\n" + "1,2,3,4,5,6,7,8,9\n\ne:" + pars.Length);
+			//Въвеждане на втори ред
+			table [1, 0] = "Крум"; table [1, 1] = "Георгиев"; table [1, 2] = "39";
 
-			String list1 = string.Join (";", pars);
-			Console.WriteLine ("Новият стринг е:\n" + list1 + "\n\n");
 
+			//Печат
+			Console.WriteLine ("Кой ред искате да видите: ");
+			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
+
+			Console.WriteLine ((table [_index, 0]) [0] + ", " + table [_index, 1] + ", " + table [_index, 2] + " г.");
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
