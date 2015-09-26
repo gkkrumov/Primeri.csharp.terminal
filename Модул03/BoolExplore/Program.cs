45e3b0696caad84a12ce4abcd098349751ed22d5
ГККрумов <geobojur@abv.bg>
2015-09-26 23:24:01 +0300

    Модул 03, Видео 08 - Използване на условно присвояване

--- Модул03/BoolExplore/Program.cs
+++ Модул03/BoolExplore/Program.cs
@@ -6,33 +6,22 @@ namespace BoolExplore
 	{
 		public static void Main (string[] args)
 		{
-			bool _b = true;  //true /false
-
-			bool _test = 5 > 10;
-
 			int[] _i = new int[] { 4, 3, 2 };
 
-			int _ui = Convert.ToInt32 (Console.ReadLine ());
+			int _ui = 0;
+			string _input = "0";
 
+			Console.WriteLine ("Моля въведете индекс: ");
+			_input = (Console.ReadLine ());
 
-			//bool _check01 = _ui <= _i.Length;
-//			bool _check02 = _ui > 0;
-//
-//			bool _exp01 = _check01 && _check02;    //Логическо и "§§"
-//			bool _exp02 = _check01 || _check02;    //Логическо или "||"
-//			bool _exp03 = !_check01;               //Отрицание "!"
-//
-//			Console.WriteLine ("Логическо И:" + _exp01.ToString ());
-//			Console.WriteLine ("Логическо ИЛИ:" + _exp02.ToString ());
-//			Console.WriteLine ("Отрицание на " + _check01.ToString () + ": " + _exp03);
+			bool _check01 = int.TryParse (_input, out _ui);
 
-			if (_ui > 0 && _ui <= _i.Length) { 
+			_ui = (_ui <= _i.Length) ? _ui : 3;
+			_ui = (_ui > 0) ? _ui : 1;
+
+			if (_check01) {
 				Console.WriteLine (_i [_ui - 1]);
-			} else if (_ui > _i.Length) {
-				Console.WriteLine ("Индексът е прекалено голям\n\n\n");
-			}
-			if (_ui <= 0) {
-				Console.WriteLine ("Индексът е прекалено малък\n\n\n");
+
 			}
 		
 		}
