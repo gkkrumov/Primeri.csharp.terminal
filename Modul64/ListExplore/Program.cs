673ec4de7386955fd588ebef459029a2c7d784c5
ГККрумов <geobojur@abv.bg>
2015-10-01 00:04:18 +0300

    Модул 64, Видео 06 - Примери с do ...While

---
+++ Modul64/ListExplore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниции
+			string _userinput = " ";
+
+			//List
+
+			//Потребителски вход
+			do {
+				Console.Write (", ");
+				_userinput = Console.ReadLine ();
+
+				//Добавяне на стойности
+
+				//Преглед на лист
+
+				//Размер на Лист
+
+			} while (_userinput != "exit");
+		}
+	}
+}
