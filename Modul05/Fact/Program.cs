e04369b4a80922d8ff203e2f05e63608d632af80
ГККрумов <geobojur@abv.bg>
2015-10-11 00:05:13 +0300

    Модул 05, Видео 08 - Рекурсивни методи

---
+++ Modul05/Fact/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fact
+{
+	class MainClass
+	{
+		//5! = 5 * 4 * 3 * 2 * 1; 0! = 1;
+
+		public static int Fact ( int _fact )
+		{
+			if (_fact <= 1)
+				return 1;
+			else
+				return _fact * Fact (_fact - 1);
+
+			//2!   2   *   1
+			//3!   3 * 2 * 1
+
+		}
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ( Fact ( 3 ));
+		}
+	}
+}
