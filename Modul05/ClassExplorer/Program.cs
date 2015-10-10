73d044f8db12495697c50970e435d5eb08c4c109
ГККрумов <geobojur@abv.bg>
2015-10-10 16:38:27 +0300

    Модул 05, Видео 04,05,06 - Класове, дефиниране на променливи и методи в клас

---
+++ Modul05/ClassExplorer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ClassExplorer
+{
+	public class person
+	{
+		private string simpleString = "текстова промевнлива от клас";
+		private string user = "";
+
+		private string hiddenString = "";
+
+		public person ( string _ini )
+		{
+			
+			getUser (_ini);
+			setWelcomeTExt (); 
+
+		}
+
+		private void getUser (string _ini)
+		{			
+			if (_ini == "pasword1") hiddenString = "Потребител 1"; 
+			if (_ini == "pasword2") hiddenString = "Потребител 2";
+
+
+			user = hiddenString;
+		}
+		private void setWelcomeTExt ()
+		{
+			if (hiddenString.Length > 0)
+				simpleString = "Добре дошли отново, " + user + "!";
+			else
+				simpleString = "Вие нямате достъп до тази програма";
+		}
+				
+		public string getWelcomeMessage()
+		{
+			return simpleString;
+		}
+
+
+			
+	}
+
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+		{
+			Console.Write ("Парола: "); string _user = Console.ReadLine ();
+
+
+			//Дефиниране на клас
+			person _person = new person(_user);
+
+			//Достъпване на клас
+			Console.WriteLine ( _person.getWelcomeMessage() );
+
+
+
+		}
+	}
+} 
+  
