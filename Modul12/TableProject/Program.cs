4af0a609a962a79fe9e02f5efc41502c04dafc59
GKKrumov <geobojur@abv.bg>
2015-10-31 18:23:07 +0200

    Модул 12, Видео 07 - Масиви

---
+++ Modul12/TableProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TableProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			string[] row = new string[3];
+			row [0] = "Ред 1";
+			row [1] = "Ред 2";
+			row [2] = "Ред 3";
+
+			Console.WriteLine ("Масив row: " + row[0] + ", " + row[1] + ", " + row[2] + "\n");
+
+			string[] parse = "1,2,3,4,5,6,7,8,9".Split (',');
+			Console.WriteLine ("Броят на елементите в масива:\n" + "1,2,3,4,5,6,7,8,9\n\nе:" + parse.Length);
+
+			//Събиране на масив в стринг
+			string List1 = string.Join (";", parse);
+			Console.WriteLine ("Новият стринг е:\n" + List1 + "\n\n");
+
+
+		}
+	}
+}
